3d73186b361b506df9ca4107afd59e665c443960
erterfed <erterfed@gmail.com>
2017-01-30 22:31:30 +0800

    Sign in ui custom classes complete

---
+++ devSocial/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  devSocial
+//
+//  Created by erterfed on 30/1/17.
+//  Copyright © 2017 erterfed. All rights reserved.
+//
+
+import UIKit
+
+let SHADOW_GRAY: CGFloat = 120.0/255.0
+
