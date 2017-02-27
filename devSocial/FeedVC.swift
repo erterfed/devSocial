ee021bb2e5b8d7d034f0bfeb4c734ae42a8716fb
erterfed <erterfed@gmail.com>
2017-02-27 17:10:11 +0800

    Completed Firebase display

--- devSocial/FeedVC.swift
+++ devSocial/FeedVC.swift
@@ -49,11 +49,18 @@ class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
+        //let post = posts[indexPath.row]
+        //print("ert: \(post.caption)")
         let post = posts[indexPath.row]
-        print("ert: \(post.caption)")
         
-        return tableView.dequeueReusableCell(withIdentifier: "PostCell") as! PostCell
+        if let cell = tableView.dequeueReusableCell(withIdentifier: "PostCell") as? PostCell {
+            cell.configureCell(post: post)
+            return cell
+        } else {
+            return PostCell()
+        }
         
+        //return tableView.dequeueReusableCell(withIdentifier: "PostCell") as! PostCell
     }
     
     
